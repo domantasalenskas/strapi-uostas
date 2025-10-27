1359bb891e80424eef77115f4c36293e7ac74027
Domantas Alenskas <domantas.alenskas@gmail.com>
2025-10-27 15:17:53 +0200

    add socket support

--- config/database.ts
+++ config/database.ts
@@ -6,7 +6,7 @@ export default ({ env }) => {
   const connections = {
     mysql: {
       connection: {
-        connectionString: env('DATABASE_URL'),
+        socketPath: env('DATABASE_SOCKET'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
